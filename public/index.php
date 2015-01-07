7040005a30c6b491d716ff4ae2bb12282268557a
Mark Perkins <mark@allmarkedup.com>
2015-01-07 11:38:03 +0000

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Base path
+|--------------------------------------------------------------------------
+|
+| Path to the root of your site (may be different from your web root!).
+|
+*/
+
+$basePath = __DIR__ . '/..';
+
+/*
+|--------------------------------------------------------------------------
+| Path to your Composer autoload file
+|--------------------------------------------------------------------------
+|
+| You will only need to edit this if you have specified a custom location
+| for your Composer vendors directory.
+|
+*/
+
+$autoloader = __DIR__ . '/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Boot up the application
+|--------------------------------------------------------------------------
+|
+| You shouldn't need to edit anything below this line.
+|
+*/
+
+require $autoloader;
+
+$app = new Prontotype\Application($basePath);
+$status = $app->run();
+
+exit($status);
